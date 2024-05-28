2734ab5586855e604fd773353377e275341bc6bc
Takanori Kishikawa <auratus.lemma@gmail.com>
2024-05-28 23:19:55 +0900

    add setup script with powershell

---
+++ setup.ps1
@@ -0,0 +1,2 @@
+New-Item -ItemType SymbolicLink -Path $Env:APPDATA\helix\languages.toml -Target .\helix\config.toml
+New-Item -ItemType SymbolicLink -Path $Env:APPDATA\helix\languages.toml -Target .\helix\languages.toml
