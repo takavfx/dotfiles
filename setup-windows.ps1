208db405315b993eb9ba58ac4d09ba4b860e323b
Takanori Kishikawa <auratus.lemma@gmail.com>
2024-06-04 06:48:28 +0900

    change nvim setup to make symbolic link to directory

--- setup-windows.ps1
+++ setup-windows.ps1
@@ -3,4 +3,12 @@ New-Item -ItemType SymbolicLink -Path $Env:APPDATA\helix\config.toml -Target .\h
 New-Item -ItemType SymbolicLink -Path $Env:APPDATA\helix\languages.toml -Target .\helix\languages.toml
 
 # NeoVim
-New-Item -ItemType SymbolicLink -Path $Env:LOCALAPPDATA\nvim\lua\plugins\colorscheme.lua -Target nvim\lua\plugins\colorscheme.lua
+if (!(Test-Path $Env:LOCALAPPDATA\nvim)) {
+    New-Item -ItemType SymbolicLink -Path $Env:LOCALAPPDATA\nvim -Target nvim
+} else {
+    $confirmation = Read-Host "nvim directory already exists. Do you want to overwrite it? (y/n)"
+    if ($confirmation -eq "y") {
+        Remove-Item $Env:LOCALAPPDATA\nvim -Recurse
+        New-Item -ItemType SymbolicLink -Path $Env:LOCALAPPDATA\nvim -Target nvim
+    }
+}
