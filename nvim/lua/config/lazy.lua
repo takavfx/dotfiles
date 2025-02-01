61c19419c2ec4cc7e47524c43cb7665903e70acb
Takanori Kishikawa <auratus.lemma@gmail.com>
2025-02-01 22:11:55 +0900

    LazyGit の python プリセットを有効に

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -25,7 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.docker" },
-    -- { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.python" },
     -- { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
